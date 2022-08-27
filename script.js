7d1b9541a25b9f287583e3928cc8da8ac46fa6a8
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-26 21:12:41 -0400

    Add keyboard support

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-/*
-2. 
-*/
-// Clear function
 const displayBox = document.getElementById("result");
+const inputs = ['1', '2', '3', '/', '4', '5', '6', '-', '7', '8', '9', '+', '.', '0', '*'];
+const calc = ['=', 'Enter'];
 
+// Add listeners
+document.addEventListener("keydown", (event) => {
+    console.log(event.key);
+    const input = event.key;
+    if(event.key === "Backspace") {
+        displayBox.value = displayBox.value.slice(0, -1);
+    } else if(inputs.includes(input)) {
+        display(input);
+    } else if(calc.includes(input)) {
+        calculate(input);
+    }
+});
+
+// CLear function
 function clearScreen() {
     displayBox.value = "";
 }
